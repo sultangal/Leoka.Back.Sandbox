75dee64acb9773124f9f66a05ecfb0eec4f1d048
Sultan Galiullin <sultangal@outlook.com>
2024-05-16 15:54:39 +0300

    #31954729. Добавил sql скрипт, редактирующий таблицу ProjectComments

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/238.esta.ddl_alter_ProjectComments.sql
@@ -0,0 +1,12 @@
+﻿ALTER TABLE IF EXISTS "Communications"."ProjectComments"
+	DROP CONSTRAINT IF EXISTS "FK_ModerationStatuses_StatusId";
+	
+ALTER TABLE IF EXISTS "Communications"."ProjectComments"
+	DROP COLUMN IF EXISTS "ModerationStatusId";
+	
+
+ALTER TABLE IF EXISTS "Communications"."ProjectComments"
+    ADD COLUMN IF NOT EXISTS "ModerationId" BIGINT NOT NULL,
+    ADD CONSTRAINT "FK_ProjectCommentsModeration_ModerationId"
+        FOREIGN KEY ("ModerationId")
+            REFERENCES "Moderation"."ProjectCommentsModeration" ("ModerationId");
